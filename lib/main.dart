b3f083b8887ec089da7a15069a0963087c853388
afeedh <afeedh_b170282cs@nitc.ac.in>
2019-07-03 14:49:23 +0530

    01

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Text('Hello Bruhh!'),
+    );
+  }
+}
