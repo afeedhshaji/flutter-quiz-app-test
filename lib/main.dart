630f46d975173e0737a2564652bc68e7043cc2c9
afeedh <afeedh_b170282cs@nitc.ac.in>
2019-07-03 15:02:28 +0530

    02

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
