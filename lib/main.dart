c127f9cc46fd2ab28868ee2c4c5a5bcbeb120667
afeedh <afeedh_b170282cs@nitc.ac.in>
2019-07-03 15:44:18 +0530

    03

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,28 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      home: Text('Hello Bruhh!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text('What\'s your question'),
+            RaisedButton(
+              child: Text('Answer1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer2'),
+              onPressed: null,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
