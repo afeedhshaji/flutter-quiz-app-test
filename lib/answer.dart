9213904bf0c393d6b80cb17b51c18ca57be58702
afeedh <afeedh_b170282cs@nitc.ac.in>
2019-07-15 00:11:36 +0530

    Seperated further into widgets

--- lib/answer.dart
+++ lib/answer.dart
@@ -3,8 +3,9 @@ import 'package:flutter/painting.dart';
 
 class Answer extends StatelessWidget {
   final Function selectHandler;
+  final String answerText;
 
-  Answer(this.selectHandler);
+  Answer(this.selectHandler, this.answerText);
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +14,7 @@ class Answer extends StatelessWidget {
       child: RaisedButton(
         color: Colors.blue,
         textColor: Colors.white,
-        child: Text('Answer1'),
+        child: Text(answerText),
         onPressed: selectHandler,
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/answer.dart';
 import './question.dart';
+import './quiz.dart';
+import 'result.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,36 +16,46 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  final _questions = const [
+    {
+      'questionText': 'Favourite color?',
+      'answerText': ['Black', 'Blue', 'Green'],
+    },
+    {
+      'questionText': 'Favourite Animal?',
+      'answerText': ['Sheep', 'Cat', 'Lion'],
+    },
+    {
+      'questionText': 'Favourite Film?',
+      'answerText': ['Spiderman', 'Harry Potter', 'Green Mile'],
+    },
+  ];
 
   void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
+
+    // if (_questionIndex < questions.length) {
+    //   print("We have more questions");
+    // }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'Favourite color?',
-      'Favourite Animal?',
-      'Favourite Book?',
-      'Favorite Film',
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
-          children: <Widget>[
-            Question(questions[_questionIndex]),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questions: _questions,
+                questionIndex: _questionIndex,
+              )
+            : Result(),
       ),
     );
   }
