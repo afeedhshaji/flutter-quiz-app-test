5a0c45920bece7b064df75b5b25bc33b65142390
afeedh <afeedh_b170282cs@nitc.ac.in>
2019-07-15 00:27:08 +0530

    Fix : Center score

--- lib/result.dart
+++ lib/result.dart
@@ -20,8 +20,11 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(resultPhrase,
-          style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold)),
+      child: Text(
+        resultPhrase,
+        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
